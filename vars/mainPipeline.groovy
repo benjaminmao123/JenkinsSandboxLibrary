e0584336d50b7f32093f9f038dd6f637569564ec
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2024-10-29 11:11:29 -0700

    Update mainPipeline.groovy

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -4,22 +4,6 @@ import hudson.model.StringParameterDefinition
 import jenkins.model.Jenkins
 
 def call(PipelineConfiguration config) {
-    // properties([
-    //     parameters([
-    //         string(
-    //             name: 'ItemID',
-    //             trim: true,
-    //             description: 'The ID of the item to be processed',
-    //         ),
-
-    //         choice(
-    //             name: 'BuildPriority',
-    //             choices: ['5', '4', '3', '2', '1'],
-    //             description: 'Lower number means higher priority.',
-    //         ),
-    //     ])
-    // ])
-
     node
     {
         stage ('Build')
