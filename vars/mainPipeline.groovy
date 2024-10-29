d71bbbc0a6ef639f8f9d110707ee3fbe787a89f0
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2024-10-29 09:14:57 -0700

    Update mainPipeline.groovy

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -24,30 +24,8 @@ def call(PipelineConfiguration config) {
 
     node
     {
-        stage ('Clean Workspace')
-        {
-            cleanWs()
-        }
-
-        stage ('Checkout')
-        {
-            checkout scm
-        }
-
         stage ('Build')
         {
-            // Specify your solution or project file
-            // def solutionFile = 'JenkinsSandbox.sln'
-
-            // try {
-            //     bat "\"${tool 'MSBuild'}\" ${solutionFile} /p:Configuration=Release /p:Platform=x64 /t:Build"
-            // } catch (Exception e) {
-            //     echo "Build failed: ${e}"
-            //     currentBuild.result = 'FAILURE'
-            //     error "MSBuild failed"
-            // }
-            
-            // introduce artificial delay
             sleep 30
         }
     }
